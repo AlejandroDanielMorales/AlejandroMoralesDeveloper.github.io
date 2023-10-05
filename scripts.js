0d8738d47e6186189f01a0895375071f0e38f703
AlejandroDanielMorales <116897330+AlejandroDanielMorales@users.noreply.github.com>
2023-10-05 14:48:53 -0300

    Update scripts.js
    Fff

--- scripts.js
+++ scripts.js
@@ -5,37 +5,45 @@ function cambiarImagenes(numero) {
   // Obtener todas las imágenes del carrusel dentro del elemento del carrusel
   var imagenes = carrusel.querySelectorAll('.carousel-item img');
 
-  // Definir las rutas de las imágenes para cada conjunto (1 al 4)
+  // Obtener el div del carrusel para aplicar la transición de opacidad
+  var carouselDiv = carrusel.querySelector('.carousel-inner');
 
-  // Verificar si el número está dentro del rango válido (1-4)
-  if (numero >= 1 && numero <= 4) {
-    // Actualizar las rutas de las imágenes en el carrusel
-    switch (numero) {
+  // Aplicar una clase CSS para la transición de opacidad
+  carouselDiv.classList.add('fade-out');
+
+  // Esperar a que termine la transición antes de cambiar las imágenes
+  setTimeout(function () {
+    // Definir las rutas de las imágenes para cada conjunto (1 al 4)
+    if (numero >= 1 && numero <= 4) {
+      // Actualizar las rutas de las imágenes en el carrusel
+      switch (numero) {
         case 1:
-        imagenes[0].setAttribute('src', 'https://thumbs.dreamstime.com/b/transparent-pattern-background-simulation-alpha-channel-png-seamless-gray-white-squares-vector-design-grid-162521286.jpg?w=450');
-        imagenes[1].setAttribute('src', 'https://thumbs.dreamstime.com/b/transparent-pattern-background-simulation-alpha-channel-png-seamless-gray-white-squares-vector-design-grid-162521286.jpg?w=450');
-        imagenes[2].setAttribute('src', 'https://thumbs.dreamstime.com/b/transparent-pattern-background-simulation-alpha-channel-png-seamless-gray-white-squares-vector-design-grid-162521286.jpg?w=450');
-        break;
-      case 2:
-        imagenes[0].setAttribute('src', 'Nonox1.png');
-        imagenes[1].setAttribute('src', 'Nonox2.png');
-        imagenes[2].setAttribute('src', 'Nonox3.png');
-        break;
-      case 3:
-        imagenes[0].setAttribute('src', 'cManager3.png');
-        imagenes[1].setAttribute('src', 'cManager2.png');
-        imagenes[2].setAttribute('src', 'cManager4.png');
-        break;
-      
-      case 4:
-        imagenes[0].setAttribute('src', 'https://img.freepik.com/vector-gratis/proximamente-estilo-grunge-abstracto-salpicaduras-negras_1017-26690.jpg?w=740&t=st=1696524217~exp=1696524817~hmac=5df3186ad6e7851d01b65899215a8f16005773cf55456ecf115459433ac062c0');
-        imagenes[1].setAttribute('src', 'https://img.freepik.com/vector-gratis/proximamente-estilo-grunge-abstracto-salpicaduras-negras_1017-26690.jpg?w=740&t=st=1696524217~exp=1696524817~hmac=5df3186ad6e7851d01b65899215a8f16005773cf55456ecf115459433ac062c0');
-        imagenes[2].setAttribute('src', 'https://img.freepik.com/vector-gratis/proximamente-estilo-grunge-abstracto-salpicaduras-negras_1017-26690.jpg?w=740&t=st=1696524217~exp=1696524817~hmac=5df3186ad6e7851d01b65899215a8f16005773cf55456ecf115459433ac062c0');
-        break;
+          imagenes[0].setAttribute('src', 'Nonox1.png');
+          imagenes[1].setAttribute('src', 'Nonox2.png');
+          imagenes[2].setAttribute('src', 'Nonox3.png');
+          break;
+        case 2:
+          imagenes[0].setAttribute('src', 'cManager2.png');
+          imagenes[1].setAttribute('src', 'cManager3.png');
+          imagenes[2].setAttribute('src', 'cManager4.png');
+          break;
+        case 3:
+          imagenes[0].setAttribute('src', 'Nonox3.png');
+          imagenes[1].setAttribute('src', 'cManager4.png');
+          imagenes[2].setAttribute('src', 'ALE.jpeg');
+          break;
+        case 4:
+          imagenes[0].setAttribute('src', 'ALE.jpeg');
+          imagenes[1].setAttribute('src', 'Nonox3.png');
+          imagenes[2].setAttribute('src', 'cManager4.png');
+          break;
+      }
+    } else {
+      // En caso de que el número esté fuera del rango, mostrar un mensaje de error
+      console.error('Número no válido. Debe estar entre 1 y 4.');
     }
-  } else {
-    // En caso de que el número esté fuera del rango, mostrar un mensaje de error
-    console.error('Número no válido. Debe estar entre 1 y 4.');
-  }
+
+    // Eliminar la clase CSS para la transición de opacidad
+    carouselDiv.classList.remove('fade-out');
+  }, 500); // Cambia este valor para ajustar la duración de la transición (en milisegundos)
 }
-  
